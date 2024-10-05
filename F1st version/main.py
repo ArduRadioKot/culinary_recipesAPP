986c1d54af6cf76b59c89e2f7d6e8881dc075980
AlexVorob <sana20112009@gmail.com>
2024-10-05 16:28:28 +0300

    upd

--- F1st version/main.py
+++ F1st version/main.py
@@ -4,14 +4,16 @@ from PyQt5.QtCore import Qt
 app = QApplication([])
 
 # Загрузка файла со стилями
-with open("styles.qss", "r") as f:
+with open('styles.qss', 'r') as f:
     stylesheet = f.read()
 
 # Применение стилей к приложению
 app.setStyleSheet(stylesheet)
 
 window = QWidget()
+window.setWindowTitle("My Application")  # Set a window title
 label = QLabel("Hello, world!", window)
+label.setAlignment(Qt.AlignCenter)  # Center the label text
 
 window.setGeometry(100, 100, 300, 150)
 window.show()
